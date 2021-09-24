cd7b720997297d10382b18885a02e57375fd2c6f
Gustaf Haglund <ghaglund@bahnhof.se>
2021-09-24 04:40:54 +0200

    innan senare fix

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,6 @@ fn main() {
     board.populate_board();
     //println!("{:?}", board.table);
 
-    /*let pMove:String = "e2e4".to_string();
-    let mut end:usize = 2;
-
-    if pMove.len() % 2 == 1 {
-        end = 3;
-    }
-
-    let startPiece:String = pMove.chars().collect::<String>()[..2].to_string();
-    let endPiece:String = pMove.chars().collect::<String>()[end..].to_string();
-
-    println!("{:?} {:?}", startPiece, endPiece); */
-
     board.table[1][3] = None;
 
     println!("{:?}", board.table[0][4]);
@@ -24,7 +12,7 @@ fn main() {
     let mut notat: AlgebraicNotation = Notation::new(board, schackmotor::_WHITE_PIECE);
     //notat.do_move("Be1xe5");
     notat.do_move("Bc1d2");
-    notat.do_move("Ke1xd2");
+    //notat.do_move("Ke1xd2");
 
     //let x = schackmotor::Pawn {};
 }
